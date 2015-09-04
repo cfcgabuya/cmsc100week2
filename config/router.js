8815b060966c282c4151237eece46e71e457eae8
cfcgabuya <christinegabuya@gmail.com>
2015-09-04 12:35:39 +0800

    CMSC 100 Week 3: Added MySQL module and functions.

--- config/router.js
+++ config/router.js
@@ -3,14 +3,18 @@ var teacher = require('./../controllers/teachers');
 
 module.exports = function(router){
 	router.route('/students')
-		.get(student.find);
+		.get(student.find)
+		.post(student.insert);
+		
+	router.route('/students/:id')
+		.get(student.findOne)
+		.put(student.update)
+		.delete(student.remove);
+		
 	router.route('/teachers')
-		.get(teacher.find);
-	router.route('/teachers')
-		.post(teacher.create);
-	router.route('/teachers')
-		.put(teacher.update);
-	router.route('/teachers')
+		.get(teacher.find)
+		.post(teacher.create)
+		.put(teacher.update)
 		.delete(teacher.delete);
 	return router;
 };
